26b86631665107aca07b8ea4466c5303902b4174
Khrystyna Khutka <kkhutka@gmail.com>
2023-11-14 23:18:30 -0600

    without resizable delete

--- src/components/add-film-form/AddFilm.js
+++ src/components/add-film-form/AddFilm.js
@@ -18,15 +18,29 @@ const Add = () =>{
     const [companyId, setCompanyId] = useState(0);
     const [type, setType] = useState("");
 
-
+    const [iconError, setIconError] = useState("");
+    const [nameError, setNameError] = useState("");
+    const [yearError, setYearError] = useState("");
+    const [typeError, setTypeError] = useState("");
     const handleNameChange = event => {
-        setName(event.target.value);
+        const inputValue = event.target.value;
+        setName(inputValue);
+        if (!inputValue) {
+            setNameError("Поле не може бути порожнім");
+        } else {
+            setNameError("");
+        }
     }
 
     const handleYearChange = event => {
-        const year = +event.target.value;
-        if (!isNaN(year)) {
-            setYear(year);
+        const inputValue = +event.target.value;
+        setYear(inputValue);
+
+        // Валідація числового значення
+        if (isNaN(inputValue) || inputValue < 0) {
+            setYearError("Введіть коректний рік");
+        } else {
+            setYearError("");
         }
     }
     const handleActorsChange = selectedActorOptions => {
@@ -55,7 +69,16 @@ const Add = () =>{
     }));
 
     const handleTypeChange = event => {
-        setType(event.target.value);
+        const inputValue = event.target.value;
+        setType(inputValue);
+
+        // Валідація на мінімальну довжину тексту
+        const minLength = 3;
+        if (inputValue.length < minLength) {
+            setTypeError(`Мінімум ${minLength} символи`);
+        } else {
+            setTypeError("");
+        }
     }
     const addFilm = () => {
         const film = {icon, name, year, actorsIds, companyId, type};
@@ -72,8 +95,16 @@ const Add = () =>{
         }),
     };
     const handleIconChange = event => {
+        const inputValue = event.target.value;
+        setIcon(inputValue);
 
-        setIcon(event.target.value);
+        // Валідація URL
+        const urlRegex = /^(ftp|http|https):\/\/[^ "]+$/;
+        if (inputValue && !urlRegex.test(inputValue)) {
+            setIconError("Введіть коректний URL");
+        } else {
+            setIconError("");
+        }
 
     }
 
@@ -81,9 +112,17 @@ const Add = () =>{
         <div className={'container'}>
             <div className='add-film'>
                 <label className={'add-film_label'}>Icon url:</label><input  className={'add-film_input'} type='text' value={icon} onChange={handleIconChange} placeholder={'URL...'}/>
+                <span className="error-message">{iconError}</span>
+
                 <label className={'add-film_label'}>Name:</label><input  className={'add-film_input'} type='text' value={name} onChange={handleNameChange} placeholder={'Name...'}/>
+                <span className="error-message">{nameError}</span>
+
                 <label className={'add-film_label'}>Year:</label><input className={'add-film_input'} type='text' value={year} onChange={handleYearChange} placeholder={'Year...'}/>
+                <span className="error-message">{yearError}</span>
+
                 <label className={'add-film_label'}>Type:</label><input className={'add-film_input'} type='text' value={type} onChange={handleTypeChange} placeholder={'Type...'}/>
+                <span className="error-message">{typeError}</span>
+
                 <label className={'add-film_label'}>Actors:</label><Select styles={customStyles} options={optionsActors} defaultValue={selectedActorOptions}
                                                    onChange={handleActorsChange} isMulti/>
                 <label className={'add-film_label'}>Company:</label> <div  className={'company_radio_buttons' }>{optionsCompanies.map(company => (
--- src/components/delete_window/delete_window.js
+++ src/components/delete_window/delete_window.js
@@ -13,7 +13,7 @@ import ResizePanel from "react-resize-panel";
 
 const Delete = () =>{
     const [windowState, setWindowState] = useState({ width: 320, height: 200 });
-    const [position, setPosition] = useState({ x: 0, y: 0 });
+    const [position, setPosition] = useState({ x: (window.innerWidth-200)/2, y: (window.innerHeight-100)/2 });
 
 
     const navigate = useNavigate();
@@ -26,59 +26,30 @@ const Delete = () =>{
         navigate('/')
     }
 
-
-
-    // const handleResizeStart = () => {
-    //     setResizing(true);
-    // };
-    //
-    // const handleResizeStop = () => {
-    //     setResizing(false);
-    // };
-    //
-    // const handleMouseMove = (e) => {
-    //     if (resizing) {
-    //         setWidth(e.clientX - resizableRef.current.getBoundingClientRect().left);
-    //         setHeight(e.clientY - resizableRef.current.getBoundingClientRect().top);
-    //     }
-    // };
-    //
-    // const handleResize = (e, { size }) => {
-    //     setWidth(size.width);
-    //     setHeight(size.height);
-    // };
-    //
     const handleDrag = (e, { x, y }) => {
         setPosition({ x, y });
     };
-    const resizableRef = useRef(null);
 
     return(
 
-        // <Resizable
-        //     style={{ marginLeft: 100, marginTop: 100, border: "1px solid black" }}
-        //     size={{ width: windowState.width, height: windowState.height }}
-        //     onResizeStop={(e, direction, ref, d) => {
-        //         setWindowState({
-        //             width: windowState.width + d.width, height: windowState.height + d.height,});
-        //     }}>
-        <ResizePanel direction="w" style={{ width: '400px' }} handleClass='customHandle' borderClass='customResizeBorder'>
+
         <Draggable handle=".handle"
                    defaultPosition={{ x: (window.innerWidth-200)/2, y: (window.innerHeight-100)/2 }}
-                   position={{ x: (window.innerWidth-360)/2, y: (window.innerHeight-100)/2 }}
+                   position={position}
                    bounds={{ left: 0, top: 0, right: window.innerWidth - 360, bottom: window.innerHeight - 100 }}
                    onStop={handleDrag}>
                 <div className="window">
                     <div className="handle">
                     <div className="content">
-                        <p>Resizable Content</p>
-                        <Button text={'Cancel'} color={'yellow'} lnk={()=>navigate(-1)}/>
-                        <Button text={'Delete anyway'} color={'red'} lnk={deleteFilm}/>
+                        <p style={{fontFamily:"Comic Sans MS", textAlign: "center"}}>Are you sure?</p>
+                        <div className={'delete_buttons'}>
+                            <Button text={'Cancel'} color={'#eabe0d'} lnk={()=>navigate(-1)}/>
+                            <Button text={'Delete anyway'} color={'red'} lnk={deleteFilm}/>
+                        </div>
                     </div>
                     </div>
                 </div>
         </Draggable>
-        </ResizePanel>
 
 
     )
